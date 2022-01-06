dda31fa47db99d9bd16b773d08659fede58ec723
Nikita Maximov <john.maks595@gmail.com>
2022-01-06 13:16:43 +0300

    add setParametrs

--- src/js/libs/gallery.js
+++ src/js/libs/gallery.js
@@ -1,5 +1,68 @@
+const GalleryClassName = 'gallery';
+const GalleryLineClassName = 'gallery-line';
+const GallerySlideClassName = 'gallery-slide';
+
 class Gallery {
     constructor(element, options = {}) {
-        this.containerNode = element
+        this.containerNode = element;
+        this.size = element.childElementCount;
+        this.currentSlide = 0;
+
+        this.manageHTML = this.manageHTML.blind(this);
+        this.setParameters = this.setParameters.blind(this);
+        this.setEvents = this.setEvents.bind(this);
+        this.resizeGallery = this.resizeGallery.bind(this);
+
+        this.manageHTML();
+        this.setParameters();
+        this.setEvents();
+    }
+
+    manageHTML() {
+        this.containerNode.classList.add(GalleryClassName);
+        this.containerNode.innerHtml = `
+            <div class="${GalleryLineClassName}">
+                ${this.containerNode.innerHtml}
+            <div>
+        `;
+        this.lineNode = this.containerNode.querySelector(`.${GalleryLineClassName}`);
+
+        this.slideNodes = Array.from(this.lineNode.children).map((childNode) =>
+            wrapElementByDiv({
+                element: childNode,
+                className: GallerySlideClassName
+            })
+        );
+    }
+
+    setParameters() {
+        const coordsContainer = this.containerNode.getBoundingClientRect();
+        this.width = coordsContainer.width;
+
+        this.lineNode.style.width = `${this.size * this.width}px`;
+        Array.from(this.slideNodes).forEach((slideNode) => {
+            slideNode.style.width = `${this.width}px`
+        }); 
+    }
+
+    setEvents() {
+        window.addEventListener('resize', debounce(this.resizeGallery));
+    }
+    resizeGallery() {
+        this.setParameters();
     }
 }
+
+//Helpers
+function wrapElementByDiv({element, className}) {
+    const wrapperNode = document.createElement('div');
+    wrapperNode.classList.add(className)
+
+    element.parentNode.insertBefore(wrapperNode);
+    wrapperNode.appendChild(element);
+
+    return wrapperNode;
+}
+
+function debounce(func,)
+////////////14.44
