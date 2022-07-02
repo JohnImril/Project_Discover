dcdaede4795e0264803f3846c4aba0468003f1aa
Nikita Maximov <john.maks595@gmail.com>
2022-07-02 22:10:55 +0300

    add burger

--- static/js/index.js
+++ static/js/index.js
@@ -2,3 +2,11 @@ new Gallery(document.getElementById("gallery"), {
 	margin: 10,
 	dots: false,
 });
+
+const MENU_OPENED_CLASS = "burger-menu__opened";
+const burgerNode = document.querySelector(".burger-menu");
+const burgerControlNode = document.querySelector(".burger-menu__control");
+
+burgerControlNode.addEventListener("click", (_event) => {
+	burgerNode.classList.toggle(MENU_OPENED_CLASS);
+} )
