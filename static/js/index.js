dc9f009740c0010604be75fc39dea1f39913c548
Nikita Maximov <john.maks595@gmail.com>
2022-07-03 16:29:41 +0300

    resize

--- static/js/index.js
+++ static/js/index.js
@@ -3,10 +3,36 @@ new Gallery(document.getElementById("gallery"), {
 	dots: false,
 });
 
-const MENU_OPENED_CLASS = "burger-menu__opened";
-const burgerNode = document.querySelector(".burger-menu");
+/**
+ * Burger
+ */
+
+const BODY_BURDER_OPENED = "body_burder-opened";
+
+const bodyNode = document.querySelector("body");
 const burgerControlNode = document.querySelector(".burger-menu__control");
+const burgerSpaceCoverNode = document.querySelector(".cover-space__burger");
 
 burgerControlNode.addEventListener("click", (_event) => {
-	burgerNode.classList.toggle(MENU_OPENED_CLASS);
-} )
+	if (bodyNode.classList.contains(BODY_BURDER_OPENED)) {
+		bodyNode.classList.remove(BODY_BURDER_OPENED);
+	} else {
+		bodyNode.classList.add(BODY_BURDER_OPENED);
+	}
+});
+
+burgerSpaceCoverNode.addEventListener("click", (_event) => {
+	bodyNode.classList.remove(BODY_BURDER_OPENED);
+});
+
+const HEADER_SCROLLED_CLASS = "header_scrolled";
+const headerNode = document.querySelector(".header");
+const stickyStartScroll = 20;
+
+window.addEventListener("scroll", () => {
+	if (window.scrollY >= 20 && !headerNode.classList.contains(HEADER_SCROLLED_CLASS)) {
+		headerNode.classList.add(HEADER_SCROLLED_CLASS);
+	} else if (window.scrollY < 20 && headerNode.classList.contains(HEADER_SCROLLED_CLASS)) {
+		headerNode.classList.remove(HEADER_SCROLLED_CLASS);
+	}
+});
